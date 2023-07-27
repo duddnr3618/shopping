cffb1d1466ca33c71d5ccf5343617da419cb7384
Youngwook Choi <duddnr3618@gmail.com>
2023-07-28 00:08:51 +0900

    1차 : 엔티티 어노테이션을 이용해 테이블의 제약 및 컬럼 지정 테스트

--- src/main/java/com/mysite/shoppingmall/test/entitiy/Test.java
+++ src/main/java/com/mysite/shoppingmall/test/entitiy/Test.java
@@ -1,14 +1,28 @@
 package com.mysite.shoppingmall.test.entitiy;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Test {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String name;
-	private int age;
+	@Column(name="name" , nullable = false , length = 20 , insertable=true)
+	private String myName;
+	
+	@Column(insertable=true)
+	private int myAge;
+	
+	@Column(insertable=true)
+	private String myInfo;
+	
+	
+	
+	
 }
