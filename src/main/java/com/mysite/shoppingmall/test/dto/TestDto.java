62a93ebd232d505170b7c49f3a35d84d24085f8d
Youngwook Choi <duddnr3618@gmail.com>
2023-07-27 21:15:50 +0900

    1차 : DB와 연결후 테이블 생성 및  test 완료

--- src/main/java/com/mysite/shoppingmall/test/dto/TestDto.java
+++ src/main/java/com/mysite/shoppingmall/test/dto/TestDto.java
@@ -3,8 +3,22 @@ package com.mysite.shoppingmall.test.dto;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TestDto {
 
+	private String name;
+	private int age;
+	
 }
--- src/main/java/com/mysite/shoppingmall/test/entitiy/Test.java
+++ src/main/java/com/mysite/shoppingmall/test/entitiy/Test.java
@@ -1,7 +1,14 @@
 package com.mysite.shoppingmall.test.entitiy;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Test {
 
+	@Id
+	private Long id;
+	
 	private String name;
 	private int age;
 }
